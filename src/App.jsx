ce368a84eee85de68b78769bf7bb43662771a4d1
carlos222t <carlos.thomas@ciac.edu.mx>
2026-05-05 21:47:23 -0500

    Add Vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react"
 import { Toaster } from "@/components/ui/toaster"
 import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClientInstance } from '@/lib/query-client'
@@ -72,6 +73,7 @@ function App() {
           </TutorialProvider>
         </Router>
         <Toaster />
+      <Analytics />
       </QueryClientProvider>
     </AuthProvider>
   )
